2d00f3d9cbf86a9c6e1c7152876325892f19451d
zhenyauk <zhenyauk@gmail.com>
2020-02-11 17:47:56 +0200

    ss

--- app/Http/Controllers/Admin/StatementController.php
+++ app/Http/Controllers/Admin/StatementController.php
@@ -60,7 +60,7 @@ class StatementController extends Controller
         Mail::to($email)
             ->send(new SendMail($data));
 
-        echo "Отправлен : " .  "<a href=' " . public_path() . '/hello2.pdf' . "'>пример файла</a>;
+        echo "Отправлен : "  .   public_path() .  "/hello2.pdf" ;
     }
 
 
