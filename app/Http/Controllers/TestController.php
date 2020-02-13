e6495fb1a765c5f3b2e30b0da51560f84bc0a7a7
zhenyauk <zhenyauk@gmail.com>
2020-02-13 20:33:35 +0200

    sd

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -18,47 +18,7 @@ class TestController extends Controller
 {
     public function index(Request $filters)
     {
-
-
-        dd('ok');
-
-        //$zip = new ZipArhive();
-
-
-        /*
-        $zip->open('files.zip', ZIPARHIVE::CREATE);
-
-        $file = file_get_contents(public_path() . "/print.pdf");
-        $zip->setPassword("MySecretPassword");
-        $f_name = public_path() . "1111.zip";
-        $zip->addFromString($f_name, $file);
-        $zip->setEncryptionName('file22.zip', ZipArchive::EM_AES_256);
-        $zip->close();
-*       /
-
-
-        dd('okk');
-
-
-
-        dd('ok');
-        $file =  public_path() . "/hello2.pdf";
-
-        $zip = Zip::create(public_path() . '/file22.zip');
-        $zip->setPassword('1q2w3e4r5t');
-        $zip->setEncryptionName('file22.zip', ZipArchive::EM_AES_256);
-        $zip->add(public_path() . "/hello2.pdf");
-
-
-        $zip->close();
-
-        dd('ok');
-
-       //$trans = Transaction::paginate(10);
-
-        //return $this->makePdf($trans);
-
-
+        return 'hello';
     }
 
     public function makePdf($trans)
@@ -95,16 +55,6 @@ class TestController extends Controller
         //return view('admin.pages.test');
 
 
-        /*
-        return $this->mail();
-
-        return $pdf->download('hello.pdf');
-
-        $html =  view( 'admin.pages.payment.done' )->render();
-        $pdf = \App::make('dompdf.wrapper');
-        $pdf->loadHTML($html);
-        return $pdf->stream();
-        */
     }
 
 }
